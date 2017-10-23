fbbbdc0ef47fcf7b46515eee68d48fb630338377
Justin Minsk <justin.minsk@gmail.com>
2017-10-23 16:25:04 -0400

    Added folders and my most recent work for class
    Signed-off-by: Justin Minsk <justin.minsk@gmail.com>

--- functions/Lec14/read_smallest.py
+++ functions/Lec14/read_smallest.py
@@ -1,4 +1,4 @@
-import time_series_read # so we can use functions inside time_series_read.py
+import time_series_read
 def smallest_value(reader):
     """ (file open for reading) -> NoneType
     Read and process reader and return the smallest value after the
@@ -7,12 +7,14 @@ def smallest_value(reader):
     line = time_series_read.skip_header(reader).strip()
     # Now line contains the first data value; this is also the smallest value
     # found so far, because it is the only one we have seen.
-    smallest = int(line)
+    smallest = int(line) # first value always expected to be a number
     for line in reader:
-        value = int(line.strip())
-        # If we find a smaller value, remember it.
-    if value < smallest:
-        smallest = value
+        line = line.strip()
+        if line != '-':
+            value = int(line.strip())
+            # If we find a smaller value, remember it.
+            if value < smallest:
+                smallest = value
     return smallest
 if __name__ == '__main__':
     with open('hopedale.txt', 'r') as input_file:
